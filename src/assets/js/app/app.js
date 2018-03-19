c6d24f7bdfcbb9a91531bbd3adcb718b49a5ba7b
Samet Şafak <sametsafak@Samet-MacBook-Air.local>
2018-03-19 17:23:54 +0300

    app js comment

--- src/assets/js/app/app.js
+++ src/assets/js/app/app.js
@@ -8,3 +8,10 @@ setInterval(() => {
 function asd() {
   console.log('Hey! I am normal function!');
 }
+
+
+
+
+// We're super 😸  excited that you're trying to use ES2017+ syntax, but instead of making more yearly presets 😭 , Babel now has a better preset that we recommend you use instead: npm install babel-preset-env --save-dev. preset-env without options will compile ES2015+ down to ES5 just like using all the presets together and thus is more future proof. It also allows you to target specific browsers so that Babel can do less work and you can ship native ES2015+ to user 😎 ! We are also in the process of releasing v7, so please give http://babeljs.io/blog/2017/09/12/planning-for-7.0 a read and help test it out in beta! Thanks so much for using Babel 🙏, please give us a follow on Twitter @babeljs for news on Babel, join slack.babeljs.io for discussion/development and help support the project at opencollective.com/babel
+
+// 🙌  Thanks for using Babel: we recommend using babel-preset-env now: please read babeljs.io/env to update!
